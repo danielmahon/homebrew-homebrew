ff149458d9231bec93c4b49e49e5d2db04473466
Daniel Mahon <danielmahon@users.noreply.github.com>
2015-03-12 12:26:14 -0400

    Update mackup.rb

--- mackup.rb
+++ mackup.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Mackup < Formula
   homepage "https://github.com/danielmahon/mackup"
-  url "https://github.com/danielmahon/mackup/archive/0.8.3.tar.gz"
+  url "https://github.com/danielmahon/mackup", :using => :git
   sha1 "09e455ab0e8efec8500ba07fef3e8b3633c13d28"
 
   head "https://github.com/danielmahon/mackup.git"
